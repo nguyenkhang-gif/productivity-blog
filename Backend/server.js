fe038eb483ca244a162ce2571546889625ce0de2
Khang Nguyen <khangnguyen@MacBook-Air-cua-Khang.local>
2024-05-21 14:53:44 +0700

    sthng wrong

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import epubRoute from "./routes/epub.routes.js";
-import bodyParser from "body-parser";
+
 import path from "path";
 const app = express();
 
